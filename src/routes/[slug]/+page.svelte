ba2aa70bd6e1deb79a880ecacbbd884c25c24713
zoepje <zoe.penner@hva.nl>
2024-09-06 22:01:31 +0200

    vercel ready

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { formatDate } from '$lib/utils';
-
   export let data
 </script>
 
--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,5 @@
+import adapter from '@sveltejs/adapter-vercel'
+
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
