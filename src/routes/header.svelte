e394a2f15336c4b29c4aae3c0fa355a52f87db42
zoepje <zoe.penner@hva.nl>
2024-09-06 23:53:46 +0200

    edit

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -12,7 +12,7 @@
       <a href="/about">About</a>
     </li>
     <li>
-      <a href="/contact">Contact kjkj</a>
+      <a href="/contact">Contact</a>
     </li>
     <li>
       <a href="/rss.xml" target="_blank">RSS</a>
