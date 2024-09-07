7cd40e84459b481085fbab1c686d3293b42efc46
zoepje <zoe.penner@hva.nl>
2024-09-08 00:55:59 +0200

    Ik weet het gwn ff niet meer
    l;kasjdfhagihsdlikjfa

--- src/routes/[slug]/+page.ts
+++ src/routes/[slug]/+page.ts
@@ -1,22 +1,9 @@
 import { error } from "@sveltejs/kit"
 
-/** @type {import('./$types').EntryGenerator} */
-export function entries() {
-	return [
-		{ slug: 'Sprint-1' },
-		{ slug: 'Sprint-2' }
-	];
-}
-
-export const prerender = true;
-
 export async function load({params} : {params:any}) {
   try {
     const post = await import(`../../sprints/${params.slug}.md`)
-
-    console.log(params.slug);
     
-
     return {
       content: post.default,
       meta: post.metadata
