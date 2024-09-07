f11af009ad135cc15face23430c73c19329bd940
zoepje <zoe.penner@hva.nl>
2024-09-08 00:50:55 +0200

    tweede poging hopefull fix

--- src/routes/[slug]/+page.ts
+++ src/routes/[slug]/+page.ts
@@ -1,10 +1,21 @@
 import { error } from "@sveltejs/kit"
 
-export const prerender = 'auto';
+/** @type {import('./$types').EntryGenerator} */
+export function entries() {
+	return [
+		{ slug: 'Sprint 1' },
+		{ slug: 'Sprint 2' }
+	];
+}
+
+export const prerender = true;
 
 export async function load({params} : {params:any}) {
   try {
     const post = await import(`../../sprints/${params.slug}.md`)
+
+    console.log(params.slug);
+    
 
     return {
       content: post.default,
