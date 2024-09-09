ee887448f23ccddc6d8144ae2ecde4cc17821b01
zoepje <zoe.penner@hva.nl>
2024-09-09 15:35:21 +0200

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,7 @@
   <ul class="posts">
     {#each data.posts as post }
       <li class="post">
-        <a href="{post.slug}" class="title">{post.title}</a>
+        <a href="/Sprint-1" class="title">{post.title}</a>
         <p class="date">{formatDate(post.date)}</p>
         <p class="description">{post.description}</p>
       </li>
