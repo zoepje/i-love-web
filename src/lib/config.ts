21ff6713ee036cf413a44be5ff0922044645b1ea
zoepje <zoe.penner@hva.nl>
2024-09-12 11:44:33 +0200

    maybe a fix

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,4 @@ import { dev } from '$app/environment'
 
 export const title = 'Zoepje'
 export const description = 'Learing journal Jaar 2'
-export const url = dev ? 'http://localhost:5173/' : 'https://zoepje.xyz/'
+export const url = dev ? 'http://localhost:5173/' : 'https://i-love-web.vercel.app/'
