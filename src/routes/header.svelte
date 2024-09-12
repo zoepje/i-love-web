970cb95ce828c173df90492bc3725662c9307315
zoepje <zoe.penner@hva.nl>
2024-09-12 16:36:15 +0200

    resolves #3

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -1,22 +1,37 @@
 <script lang="ts">
-  import * as config from '$lib/config'
 	import Toggle from './toggle.svelte';
+
+  let current = 0;
 </script>
 
 <nav>
-  <a href="/" class="title">
-    <b>Journal</b>
+  <a href="/" 
+     on:click="{() => current = 0}"
+     class="title" class:active="{current === 0}">
+    Journal
   </a>
 
   <ul class="links">
     <li>
-      <a href="/experimenten">Experimenten</a>
+      <a href="/experimenten"
+         on:click="{() => current = 1}"
+         class:active="{current === 1}">
+        Experimenten
+      </a>
     </li>
     <li>
-      <a href="/about">About</a>
+      <a href="/about"
+         on:click="{() => current = 2}"
+         class:active="{current === 2}">
+        About
+      </a>
     </li>
     <li>
-      <a href="/contact">Contact</a>
+      <a href="/contact"
+         on:click="{() => current = 3}"
+         class:active="{current === 3}">
+        Contact
+      </a>
     </li>
   </ul>
 
@@ -28,6 +43,11 @@
     padding-block: var(--size-4);
   }
 
+  .title {
+    font-size: var(--font-size-4);
+    font-weight: bold;
+  }
+
   .links {
     list-style: none;
     margin-block: var(--size-7);
@@ -36,6 +56,10 @@
   a {
     color: inherit;
     text-decoration: none;
+  }
+
+  .active {
+    color: var(--color-1);
   }
 
   @media (min-width: 768px) {
