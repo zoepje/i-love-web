938f74cc199b517d3abab19140b02e426e7ecab6
zoepje <zoe.penner@hva.nl>
2025-03-11 11:19:59 +0100

    change styling per cat

--- src/routes/notes/[slug]/+page.svelte
+++ src/routes/notes/[slug]/+page.svelte
@@ -1,30 +1,34 @@
 <script lang="ts">
-	import * as config from '$lib/config'
-
-  export let data
+	import * as config from '$lib/config';
+	import { formatDate } from '$lib/utils';
+	export let data;
+	let categoryClass = data.meta.categories.includes('sprint') ? 'sprint' : '';
 </script>
 
 <svelte:head>
-  <title>{config.title} - {data.meta.title}</title>
-  <meta property="og:type" content="article" />
+	<title>{config.title} - {data.meta.title}</title>
+	<meta property="og:type" content="article" />
 	<meta property="og:title" content={data.meta.title} />
 </svelte:head>
 
-<article>
-  <hgroup>
-    <h1>{data.meta.title}</h1>
-    <p>{data.meta.description}</p>
-  </hgroup>
+<article class={categoryClass}>
+	<hgroup>
+		<h1>{data.meta.title}</h1>
+		<p>{data.meta.description}</p>
+		{#if data.meta.lastUpdated}
+			<p>Updated: {formatDate(data.meta.lastUpdated)}</p>
+		{/if}
+	</hgroup>
 
-  <div class="tags">
-    {#each data.meta.categories as category}
-      <span class="surface-4">&num;{category}</span>
-    {/each}
-  </div>
+	<div class="tags">
+		{#each data.meta.categories as category}
+			<span class="surface-4">&num;{category}</span>
+		{/each}
+	</div>
 
-  <div class="prose">
-    <svelte:component this={data.content} />
-  </div>
+	<div class="prose">
+		<svelte:component this={data.content} />
+	</div>
 </article>
 
 <style>
@@ -37,7 +41,7 @@
 		text-transform: capitalize;
 	}
 
-	h1 + p {
+	hgroup p {
 		margin-top: var(--size-2);
 		color: var(--text-2);
 	}
